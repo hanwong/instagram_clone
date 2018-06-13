e5b8117a4fd47895a298ad94d63c5295e763486d
hanwong <hanationbear@gmail.com>
2018-06-13 12:54:07 +0900

    responsive styles

--- src/components/AuthForms/index.js
+++ src/components/AuthForms/index.js
@@ -15,8 +15,7 @@ export const LoginForm = props => (
     </form>
     <span className={styles.divider}>or</span>
     <span className={styles.facebookLink}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" /> Log in
-      with Facebook
+      <Ionicon icon="logo-facebook" fontSize="20px" color="#385185" /> Log in with Facebook
     </span>
     <span className={styles.forgotLink}>Forgot password?</span>
   </div>
@@ -28,8 +27,7 @@ export const SignupForm = props => (
       Sign up to see photos and videos from your friends.
     </h3>
     <button className={styles.button}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="white" /> Log in with
-      Facebook
+      <Ionicon icon="logo-facebook" fontSize="20px" color="white" /> Log in with Facebook
     </button>
     <span className={styles.divider}>or</span>
     <form className={styles.form}>
