cfe484a69295414a0c7628b91c59cf523c063a31
hanwong <hanationbear@gmail.com>
2018-06-16 16:49:08 +0900

    Controlling inputs and submit on LoginForm

--- src/components/LoginForm/presenter.js
+++ src/components/LoginForm/presenter.js
@@ -5,14 +5,31 @@ import styles from "shared/formStyles.scss"
 
 const LoginForm = (props, context) => (
   <div className={styles.formComponent}>
-    <form className={styles.form}>
-      <input type="text" placeholder="Username" className={styles.textInput} />
+    <form
+      className={styles.form}
+      onSubmit={props.handleSubmit}
+    >
+      <input 
+        type="text"
+        name="username"
+        placeholder="Username"
+        className={styles.textInput} 
+        value={props.usernameValue}
+        onChange={props.handleInputChange}
+      />
       <input
         type="password"
+        name="password"
         placeholder="Password"
         className={styles.textInput}
+        value={props.passwordValue}
+        onChange={props.handleInputChange}
       />
-      <input type="submit" value="Log in" className={styles.button} />
+      <input
+        type="submit"
+        value="Log in"
+        className={styles.button}
+      />
     </form>
     <span className={styles.divider}>or</span>
     <span className={styles.facebookLink}>
@@ -21,6 +38,13 @@ const LoginForm = (props, context) => (
     <span className={styles.forgotLink}>Forgot password?</span>
   </div>
 )
+
+LoginForm.propTypes = {
+  usernameValue: PropTypes.string.isRequired,
+  passwordValue: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired
+}
 
 LoginForm.contextTypes = {
   t: PropTypes.func.isRequired
