677d18e14b9d4cac55719e660e717d05c25d4ede
hanwong <hanationbear@gmail.com>
2018-06-20 22:58:40 +0900

    Component initial setting

---
+++ src/components/Navigation/container.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Navigation from './presenter'
+import PropTypes from 'prop-types'
+
+class Container extends Component {
+  state = {
+  }
+
+  static propTypes = {
+  }
+
+  render () {
+    return <Navigation />
+  }
+}
+
+export default Container
---
+++ src/components/Navigation/presenter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+// import styles from 'shared/formStyles.scss'
+
+const Navigation = (props, context) => (
+  <div>
+    Navigation
+  </div>
+)
+
+Navigation.propTypes = {
+}
+
+Navigation.contextTypes = {
+  t: PropTypes.func.isRequired
+}
+
+export default Navigation
